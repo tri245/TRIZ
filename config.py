4187a9106962afd1858de9a7e20d4c2d59201eb1
Tri_nho <76763671+tri245@users.noreply.github.com>
2024-06-25 16:57:49 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Basic Config (For all bots)
 userToken = ['MTE3MjEyNzc1MzI3ODA2NjcwMA'] # User Token here, multiple accounts may work. example 'token','token'
-DiscordChannel = '' # 1254763732236763156,
+DiscordChannel = '#1254763732236763156' # 1254763732236763156,
 
 #Picture spam
 DirPictures = 'C:\\Users\\Public\\Pictures\\Cheese\\Pepper Jack\\' # Picture(s) location !ALWAYS DOUBLE SLASH AND END WITH THAT DOUBLE SLASH YO!
